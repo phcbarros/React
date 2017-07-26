f5c069509f8966be236e03e4c439fe93c0d287e2
Paulo Barros <paulo.barros@lambda3.com.br>
2017-07-26 17:13:37 -0300

    Curso React + Redux - TodoApp (Frontend) - Aula 46 - Componente PageHeader

--- curso-react-redux/todo-app/frontend/src/about/about.jsx
+++ curso-react-redux/todo-app/frontend/src/about/about.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import PageHeader from '../templates/pageHeader';
 
 export default props => (
-    <h1>Sobre</h1>
+    <div>
+        <PageHeader name='Sobre' small='Nós' />
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam quaerat debitis modi perferendis soluta laborum eveniet ratione qui eius tempora minus, aperiam suscipit doloribus aliquid neque, ea, voluptatibus at dolorum.</p>
+        <h2>Missão</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam quaerat debitis modi perferendis soluta laborum eveniet ratione qui eius tempora minus, aperiam suscipit doloribus aliquid neque, ea, voluptatibus at dolorum.</p>
+    </div>
 )
--- curso-react-redux/todo-app/frontend/src/todo/todo.jsx
+++ curso-react-redux/todo-app/frontend/src/todo/todo.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'React';
 
+import PageHeader from '../templates/pageHeader';
+
 export default class Todo extends Component {
     render() {
         return (
-            <h1>Todo</h1>
+            <PageHeader name='Tarefas' small='Cadastro'/>
         )
     }
 }
