300983372e03bea3da9496d8bc4a328ce892b801
Paulo Barros <paulo.barros@lambda3.com.br>
2017-07-26 20:10:10 -0300

    Curso React + Redux - TodoApp (Frontend) - Aula 55 -Consulta e Exclusão de TODOs

--- curso-react-redux/todo-app/frontend/src/todo/todo.jsx
+++ curso-react-redux/todo-app/frontend/src/todo/todo.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'React';
-import Axios from 'axios';
+import axios from 'axios';
 
 import PageHeader from '../templates/pageHeader';
 import TodoForm from './todoForm';
@@ -14,6 +14,19 @@ export default class Todo extends Component {
 
         this.handleChange = this.handleChange.bind(this);
         this.handleAdd = this.handleAdd.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
+
+        this.refresh();
+    }
+
+    refresh() {
+        axios.get(`${URL}?sort=-createAt`)
+            .then(result => this.setState({...this.state, description: '', list: result.data }));
+    }
+
+    handleDelete(todo) {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(() => this.refresh());
     }
 
     handleChange(e) {
@@ -23,8 +36,8 @@ export default class Todo extends Component {
     handleAdd() {
         const description = this.state.description;
 
-        Axios.post(URL, { description })
-            .then((result) => console.log('Funcionou', result.data));
+        axios.post(URL, { description })
+            .then(() => this.refresh());
     }
 
     render() {
@@ -35,7 +48,7 @@ export default class Todo extends Component {
                     handleAdd={this.handleAdd}
                     handleChange={this.handleChange}
                     description={this.state.description} />
-                <TodoList />
+                <TodoList list={this.state.list} handleDelete={this.handleDelete} />
             </div>
         )
     }
--- curso-react-redux/todo-app/frontend/src/todo/todoList.jsx
+++ curso-react-redux/todo-app/frontend/src/todo/todoList.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import IconButton from '../templates/iconButton';
 
-export default props => (
-    <div>
-        List
-    </div>
-)
+export default props => {
+
+    const renderRows = () => {
+        const list = props.list || [];
+        return list.map(todo =>
+            (
+                <tr key={todo._id}>
+                    <td>{todo.description}</td>
+                    <td>
+                        <IconButton style="danger" icon="trash-o"
+                            onClick={() => props.handleDelete(todo)} />
+                    </td>
+                </tr>
+            ))
+    }
+
+    return (
+        <table className="table table-bordered table-hover">
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                    <th>Opções</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
+    )
+}
