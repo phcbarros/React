8f9b174bb4b9207b5ed5f7923e19ca44892b5efc
Paulo Barros <paulo.barros@lambda3.com.br>
2017-07-26 17:23:46 -0300

    Curso React + Redux -TodoApp (Frontend) - Aula 47 - Componentes TodoForm e TodoList

--- curso-react-redux/todo-app/frontend/src/todo/todo.jsx
+++ curso-react-redux/todo-app/frontend/src/todo/todo.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'React';
 
 import PageHeader from '../templates/pageHeader';
+import TodoForm from './todoForm';
+import TodoList from './todoList';
 
 export default class Todo extends Component {
     render() {
         return (
-            <PageHeader name='Tarefas' small='Cadastro'/>
+            <div>
+                <PageHeader name='Tarefas' small='Cadastro' />
+                <TodoForm />
+                <TodoList />
+            </div>
         )
     }
 }
