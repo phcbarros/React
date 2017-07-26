5f49e44947ed6cc698c0a56ead1dd44f7e9e9180
Paulo Barros <paulo.barros@lambda3.com.br>
2017-07-26 19:35:34 -0300

    Curso React + Redux - TodoApp (Frontend) - Aula 54 - Evento Adicionar (Integracao Backend)

--- curso-react-redux/todo-app/frontend/src/todo/todo.jsx
+++ curso-react-redux/todo-app/frontend/src/todo/todo.jsx
@@ -1,34 +1,40 @@
 import React, { Component } from 'React';
+import Axios from 'axios';
 
 import PageHeader from '../templates/pageHeader';
 import TodoForm from './todoForm';
 import TodoList from './todoList';
 
-export default class Todo extends Component {   
-    constructor(props){
+const URL = 'http://localhost:3003/api/todos';
+
+export default class Todo extends Component {
+    constructor(props) {
         super(props);
-        this.state = { description: '', list: []};
+        this.state = { description: '', list: [] };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleAdd = this.handleAdd.bind(this);
     }
 
     handleChange(e) {
-        this.setState({...this.state, description: e.target.value });
+        this.setState({ ...this.state, description: e.target.value });
     }
 
-    handleAdd(){
-        console.log(this.state.description)
+    handleAdd() {
+        const description = this.state.description;
+
+        Axios.post(URL, { description })
+            .then((result) => console.log('Funcionou', result.data));
     }
 
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro' />
-                <TodoForm 
-                    handleAdd={this.handleAdd} 
-                    handleChange={this.handleChange} 
-                    description={this.state.description}/>
+                <TodoForm
+                    handleAdd={this.handleAdd}
+                    handleChange={this.handleChange}
+                    description={this.state.description} />
                 <TodoList />
             </div>
         )
