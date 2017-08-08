1ddf4da80c35f5a9f9ae2ae87e1f7ffc4ccebea4
Paulo Barros <paulohcbarros@outlook.com>
2017-08-08 15:06:48 -0300

    Curso React + Redux - TodoApp (Migracao para Redux) - Aula 76 - Aplicando Middleware (Redux-Promise)

--- curso-react-redux/todo-app/frontend-redux/src/todo/todoReducer.js
+++ curso-react-redux/todo-app/frontend-redux/src/todo/todoReducer.js
@@ -1,27 +1,12 @@
 import { actions } from './todoActions';
 
-const INITIAL_STATE = {
-    description: 'Comprar livro',
-    list: [{
-        _id: 1,
-        description: 'Comprar leite',
-        done: true,
-    }, {
-        _id: 2,
-        description: 'Pagar fatura cartão',
-        done: false
-    }, {
-        _id: 3,
-        description: 'Montar cadeira quando chegar',
-        done: false
-    }]
-};
+const INITIAL_STATE = { description: '', list: [] };
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case actions.DESCRIPTION_CHANGED:
             return { ...state, description: action.payload };
-        case action.TODO_SEARCHED:
+        case actions.TODO_SEARCHED:
             return { ...state, list: action.payload.data };
         default:
             return state;
