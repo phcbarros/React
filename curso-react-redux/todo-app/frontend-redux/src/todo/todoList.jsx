83ca58893663bfb320049cacef6d1a7d89c8e4d2
Paulo Barros <paulohcbarros@outlook.com>
2017-08-09 12:34:05 -0300

    Curso React + Redux - TodoApp (Migracao para Redux) - Aula 80 - Marcar como Concluído/Pendente

--- curso-react-redux/todo-app/frontend-redux/src/todo/todoList.jsx
+++ curso-react-redux/todo-app/frontend-redux/src/todo/todoList.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import IconButton from '../templates/iconButton';
+import { markAsDone, markAsPending } from './todoActions';
 
 const TodoList = props => {
 
@@ -13,9 +15,9 @@ const TodoList = props => {
                     <td className={todo.done ? 'mark-as-done' : ''}>{todo.description}</td>
                     <td>
                         <IconButton style="success" icon="check" hide={todo.done}
-                            onClick={() => props.handleMarkAsDone(todo)} />
+                            onClick={() => props.markAsDone(todo)} />
                         <IconButton style="warning" icon="undo" hide={!todo.done}
-                            onClick={() => props.handleMarkAsPending(todo)} />
+                            onClick={() => props.markAsPending(todo)} />
                         <IconButton style="danger" icon="trash-o" hide={!todo.done}
                             onClick={() => props.handleDelete(todo)} />
                     </td>
@@ -39,5 +41,6 @@ const TodoList = props => {
 }
 
 const mapStateToProps = state => ({ list: state.todo.list });
+const mapDispatchToProps = dispatch => (bindActionCreators({ markAsDone, markAsPending }, dispatch));
 
-export default connect(mapStateToProps)(TodoList);
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
