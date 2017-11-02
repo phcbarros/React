93b2ecf5bb5bfa00c2adb48c047c8585306c5694
Paulo Barros <paulo.barros@lambda3.com.br>
2017-11-02 11:23:42 -0200

    Curso React + Redux - Clicos de Pagamento (Backend) - Aula 92 - Obtendo a Qtde de Registros (Count)

--- curso-react-redux/my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ curso-react-redux/my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -1,6 +1,17 @@
-const billingCycles = require('./billingCycle');
+const BillingCycles = require('./billingCycle');
 
-billingCycles.methods(['get', 'post', 'put', 'delete']);
-billingCycles.updateOptions({ new: true, runValidators: true });
+BillingCycles.methods(['get', 'post', 'put', 'delete']);
+BillingCycles.updateOptions({ new: true, runValidators: true });
 
-module.exports = billingCycles;
+BillingCycles.route('count', (req, res, next) => {
+    BillingCycles.count((error, value) => {
+        if (error) {
+            res.status(500).json({ errors: [error] });
+        }
+        else {
+            res.json({ value });
+        }
+    });
+});
+
+module.exports = BillingCycles;
