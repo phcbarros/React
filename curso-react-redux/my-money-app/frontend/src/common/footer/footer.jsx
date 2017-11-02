ab968e3bad12fadf810312635455a0807beb8e47
Paulo Barros <paulo.barros@lambda3.com.br>
2017-11-02 16:29:59 -0200

    Curso React + Redux - Clicos de Pagamento (Frontend) - Aula 106 - Componente Rodapé

---
+++ curso-react-redux/my-money-app/frontend/src/common/footer/footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default props => (
+    <footer className="main-footer">
+        <strong>Copyright &copy; 2017</strong> 
+    </footer>
+)
--- curso-react-redux/my-money-app/frontend/src/main/app.jsx
+++ curso-react-redux/my-money-app/frontend/src/main/app.jsx
@@ -3,10 +3,17 @@ import React from 'react';
 
 import Header from '../common/header/header';
 import Sidebar from '../common/sidebar/sidebar';
+import Footer from '../common/footer/footer';
 
 export default props => (
     <div className="wrapper">
         <Header />
         <Sidebar />
+        <div className="content-wrapper">
+            <h1>
+                Conteúdo
+            </h1>
+        </div>
+        <Footer />
     </div>
 )
