7a415f65e48f4ac95c40c8dbbfa8dd98d0428e4d
Paulo Barros <paulo.barros@lambda3.com.br>
2017-11-02 16:08:58 -0200

    Curso React + Redux - Clicos de Pagamento (Frontend) - Aula 105 - Componente Menu (Parte 2)

--- curso-react-redux/my-money-app/frontend/src/common/sidebar/menu/menu.jsx
+++ curso-react-redux/my-money-app/frontend/src/common/sidebar/menu/menu.jsx
@@ -1,7 +1,12 @@
-import  React from 'react';
+import React from 'react';
+import MenuItem from './menuItem';
+import MenuTree from './menuTree';
 
 export default props => (
     <ul className="sidebar-menu">
-        
+        <MenuItem path="#" label="dashboard" icon="dashboard" />
+        <MenuTree label="Cadastro" icon="edit">
+            <MenuItem  path="#billingCycles" label="Ciclo de Pagamento" icon="usd" />
+        </MenuTree>
     </ul>
 )
