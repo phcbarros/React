886534b6093017c9d002b37d8d1bddb86cff0acc
Paulo Barros <paulo.barros@lambda3.com.br>
2017-11-02 12:26:45 -0200

    Curso React + Redux - Clicos de Pagamento (Backend) - Aula 93 - Obtendo o Sumário de Pagamentos

--- curso-react-redux/my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ curso-react-redux/my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -5,13 +5,41 @@ BillingCycles.updateOptions({ new: true, runValidators: true });
 
 BillingCycles.route('count', (req, res, next) => {
     BillingCycles.count((error, value) => {
-        if (error) {
-            res.status(500).json({ errors: [error] });
-        }
-        else {
-            res.json({ value });
-        }
+        createResponseMessage(res, error, { value });
+    });
+});
+
+BillingCycles.route('summary', (req, res, next) => {
+    //faz uma projeção e soma os créditos e debitos lançados de cada ciclo
+    const sumCreditsAndDebts = {
+        $project: { credit: { $sum: '$credits.value' }, debt: { $sum: '$debts.value' } }
+    };
+
+    //soma todos os créditos e débitos de cada ciclo
+    const groupsAndSumsAllCredtsAndDebts = {
+        $group: { _id: null, credit: { $sum: '$credit' }, debt: { $sum: '$debt' } }
+    };
+
+    //projeta com resultado os dados de credit e debt
+    const projectCreditAndDebt = { $project: { _id: 0, credit: 1, debt: 1 } };
+
+    //pipeline
+    const summaryPipeline = [sumCreditsAndDebts, groupsAndSumsAllCredtsAndDebts, projectCreditAndDebt];
+
+    BillingCycles.aggregate(summaryPipeline, (error, result) => {
+        const data = result[0] || { credit: 0, debt: 0 };
+        createResponseMessage(res, error, data);
     });
 });
 
 module.exports = BillingCycles;
+
+const createErrorMessage = (res, error) => res.status(500).json({ errors: [error] });
+const createSuccessMessage = (res, value) => res.json(value);
+
+const createResponseMessage = (res, error, value) => {
+    if (error) 
+        createErrorMessage(res, error);
+    else
+        createSuccessMessage(res, value);
+};
