c26029a33a8cd14bce87edf40c3217b223f2d4a2
Paulo Barros <paulo.barros@lambda3.com.br>
2017-11-02 15:53:15 -0200

    Curso React + Redux - Clicos de Pagamento (Frontend) - Aula 104 - Componente Menu (Parte 1)

--- curso-react-redux/my-money-app/frontend/src/main/app.jsx
+++ curso-react-redux/my-money-app/frontend/src/main/app.jsx
@@ -2,9 +2,11 @@ import '../common/template/dependencies';
 import React from 'react';
 
 import Header from '../common/header/header';
+import Sidebar from '../common/sidebar/sidebar';
 
 export default props => (
     <div className="wrapper">
         <Header />
+        <Sidebar />
     </div>
 )
