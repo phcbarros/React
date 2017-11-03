b1eb3f4901ed670d00498916309d43241fa0e7e4
Paulo Barros <paulo.barros@lambda3.com.br>
2017-11-03 19:11:53 -0200

    Curso React + Redux - Clicos de Pagamento (Frontend) - Aula 117 - Configurando Redux DevTools

--- curso-react-redux/my-money-app/frontend/src/dashboard/dashboard.jsx
+++ curso-react-redux/my-money-app/frontend/src/dashboard/dashboard.jsx
@@ -22,11 +22,11 @@ class Dashboard extends Component {
                 <Content>
                     <Row>
                         <ValueBox cols="12 4" color="green" icon="bank"
-                            value={`R$ ${credit}`} text="Total de Créditos" />
+                            value={`R$ ${credit.toFixed(2)}`} text="Total de Créditos" />
                         <ValueBox cols="12 4" color="red" icon="credit-card"
-                            value={`R$ ${debt}`} text="Total de Débitos" />
+                            value={`R$ ${debt.toFixed(2)}`} text="Total de Débitos" />
                         <ValueBox cols="12 4" color="blue" icon="money"
-                            value={`R$ ${credit - debt}`} text="Valor Consolidado" />
+                            value={`R$ ${(credit - debt).toFixed(2)}`} text="Valor Consolidado" />
                     </Row>
                 </Content>
             </div>
