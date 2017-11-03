9bc772d1233bb5b0cc09877646d8ce05ef3e02ff
Paulo Barros <paulo.barros@lambda3.com.br>
2017-11-03 18:10:15 -0200

    Curso React + Redux - Clicos de Pagamento (Frontend) - Aula 114 - Integrando Dashboard com Redux (Parte 1)

--- curso-react-redux/my-money-app/frontend/src/dashboard/dashboard.jsx
+++ curso-react-redux/my-money-app/frontend/src/dashboard/dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import ContentHeader from '../common/content-header/contentHeader';
 import Content from '../common/content/content';
@@ -7,14 +8,18 @@ import Row from '../common/layout/row';
 
 class Dashboard extends Component {
     render() {
+        const { credit, debt } = this.props.summary;
         return (
             <div>
                 <ContentHeader title="Dashboard" small="Versão 1.0" />
                 <Content>
                     <Row>
-                        <ValueBox cols="12 4" color="green" value="R$ 3000" text="Total de Créditos" icon="bank" />
-                        <ValueBox cols="12 4" color="red" value="R$ 3000" text="Total de Débitos" icon="credit-card" />
-                        <ValueBox cols="12 4" color="blue" value="R$ 3000" text="Valor Consolidado" icon="money" />
+                        <ValueBox cols="12 4" color="green" icon="bank"
+                            value={`R$ ${credit}`} text="Total de Créditos"  />
+                        <ValueBox cols="12 4" color="red" icon="credit-card"
+                            value={`R$ ${debt}`} text="Total de Débitos"  />
+                        <ValueBox cols="12 4" color="blue" icon="money"
+                            value={`R$ ${credit - debt}`} text="Valor Consolidado" />
                     </Row>
                 </Content>
             </div>
@@ -22,4 +27,6 @@ class Dashboard extends Component {
     }
 }
 
-export default Dashboard;
+//Mapeando o state para as props
+const mapStateToProps = state => ({ summary: state.dashboard.summary });
+export default connect(mapStateToProps)(Dashboard);
---
+++ curso-react-redux/my-money-app/frontend/src/dashboard/dashboardReducer.js
@@ -0,0 +1,5 @@
+const INITIAL_STATE = { summary: { credit: 0, debt: 0 } };
+
+export default (state = INITIAL_STATE, action) => {
+    return state;
+}
