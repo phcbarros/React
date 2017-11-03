7020b4769721bba2a3f1bb1e36b151949284a33b
Paulo Barros <paulo.barros@lambda3.com.br>
2017-11-03 17:37:28 -0200

    Curso React + Redux - Clicos de Pagamento (Frontend) - Aula 112 - Usando ValueBox

--- curso-react-redux/my-money-app/frontend/src/dashboard/dashboard.jsx
+++ curso-react-redux/my-money-app/frontend/src/dashboard/dashboard.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import ContentHeader from '../common/content-header/contentHeader';
 import Content from '../common/content/content';
+import ValueBox from './valueBox/valueBox';
+import Row from '../common/layout/row';
 
 class Dashboard extends Component {
     render() {
@@ -9,7 +11,11 @@ class Dashboard extends Component {
             <div>
                 <ContentHeader title="Dashboard" small="Versão 1.0" />
                 <Content>
-                    Dashboard
+                    <Row>
+                        <ValueBox cols="12 4" color="green" value="R$ 3000" text="Total de Créditos" icon="bank" />
+                        <ValueBox cols="12 4" color="red" value="R$ 3000" text="Total de Débitos" icon="credit-card" />
+                        <ValueBox cols="12 4" color="blue" value="R$ 3000" text="Valor Consolidado" icon="money" />
+                    </Row>
                 </Content>
             </div>
         );
