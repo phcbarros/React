d9e7dc4792e3005bab561edaf90cb80c5b608d78
Paulo Barros <paulo.barros@lambda3.com.br>
2017-11-03 16:28:33 -0200

    Curso React + Redux - Clicos de Pagamento (Frontend) - Aula 107 - Navegação com React-router

--- curso-react-redux/my-money-app/frontend/src/main/app.jsx
+++ curso-react-redux/my-money-app/frontend/src/main/app.jsx
@@ -4,15 +4,14 @@ import React from 'react';
 import Header from '../common/header/header';
 import Sidebar from '../common/sidebar/sidebar';
 import Footer from '../common/footer/footer';
+import Routes from './routes';
 
 export default props => (
     <div className="wrapper">
         <Header />
         <Sidebar />
         <div className="content-wrapper">
-            <h1>
-                Conteúdo
-            </h1>
+            <Routes />
         </div>
         <Footer />
     </div>
