2027956d8b881bbf35f081fbe68106b94edfa4c8
Paulo Barros <paulo.barros@lambda3.com.br>
2017-11-06 12:56:22 -0200

    Curso React + Redux - Clicos de Pagamento (Frontend) - Aula 121 - Componentes Estruturais das Abas

--- curso-react-redux/my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ curso-react-redux/my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 
 import ContentHeader from '../common/content-header/contentHeader';
 import Content from '../common/content/content';
+import Tabs from '../common/tabs/tabs';
+import TabsHeader from '../common/tabs/tabsHeader';
+import TabsContent from '../common/tabs/tabsContent';
 
 class BillingCycle extends Component {
     render() {
         return (
             <div>
                 <ContentHeader title="Ciclo de Pagamentos" small="Cadastro" />
-                <Content>>
-                    Ciclo de pagamentos
+                <Content>
+                    <Tabs>
+                        <TabsHeader>
+
+                        </TabsHeader>
+                        <TabsContent>
+
+                        </TabsContent>
+                    </Tabs>
                 </Content>
             </div>
         );
