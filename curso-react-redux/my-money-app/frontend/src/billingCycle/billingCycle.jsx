ea40e657f898e5956b604079ce15e0d8ac532d65
Paulo Barros <paulo.barros@lambda3.com.br>
2017-11-06 13:12:18 -0200

    Curso React + Redux - Clicos de Pagamento (Frontend) - Aula 122 - Componente TabHeader

--- curso-react-redux/my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ curso-react-redux/my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 import ContentHeader from '../common/content-header/contentHeader';
 import Content from '../common/content/content';
 import Tabs from '../common/tabs/tabs';
-import TabsHeader from '../common/tabs/tabsHeader';
-import TabsContent from '../common/tabs/tabsContent';
+import TabsHeader from '../common/tabs/tabsHeader/tabsHeader';
+import TabHeader from './../common/tabs/tabsHeader/tabHeader';
+import TabsContent from '../common/tabs/tabsContent/tabsContent';
 
 class BillingCycle extends Component {
     render() {
@@ -14,7 +15,10 @@ class BillingCycle extends Component {
                 <Content>
                     <Tabs>
                         <TabsHeader>
-
+                            <TabHeader icon="list" label="Listar" target="tabList" />
+                            <TabHeader icon="plus" label="Incluir" target="tabCreate" />
+                            <TabHeader icon="pencil" label="Alterar" target="tabUpdate" />
+                            <TabHeader icon="trash-o" label="Excluir" target="tabDelete" />
                         </TabsHeader>
                         <TabsContent>
 
