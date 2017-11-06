046113047536a3a0d37ec3f658e775838d408bb0
Paulo Barros <paulo.barros@lambda3.com.br>
2017-11-06 12:46:39 -0200

    Curso React + Redux - Clicos de Pagamento (Frontend) - Aula 120 - Componente BillingCycle: Refatorando para Classe

--- curso-react-redux/my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ curso-react-redux/my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default props => (
-    <div>
-        <h1>Billing Cycle</h1>
-    </div>
-)
+import ContentHeader from '../common/content-header/contentHeader';
+import Content from '../common/content/content';
+
+class BillingCycle extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title="Ciclo de Pagamentos" small="Cadastro" />
+                <Content>>
+                    Ciclo de pagamentos
+                </Content>
+            </div>
+        );
+    }
+}
+
+export default BillingCycle;
