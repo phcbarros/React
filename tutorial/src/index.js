14d9ff8efb2d001acded84ea096d803b69f53a12
Paulo Barros <paulo.barros@lambda3.com.br>
2018-11-11 00:31:24 -0200

    Adicionado destaque na linha que foi vencedora

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -3,19 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const Square = props => (
-  <button type="button" 
-    className={"square " + (props.value === 'X'? 'text-danger' : 'text-warning')} 
+  <button type="button"
+    className={"square " + (props.winnerMove? 'bg-success' : '')}
     onClick={() => props.onClick()}>
-    {props.value}
+    <span className={props.value === 'X' ? 'text-danger' : 'text-warning'}>{props.value}</span>
   </button>
 );
 
 const Board = props => {
 
   function renderSquare(i) {
+    const winnerMove = props.winnerMoves && props.winnerMoves.some((val) => val === i);
     return (
       <Square
         value={props.squares[i]}
+        winnerMove={winnerMove}
         onClick={() => props.onClick(i)}
       />
     )
@@ -64,13 +66,11 @@ class Game extends React.Component {
       ...this.state,
       history: history.concat([{ squares }]),
       stepNumber: history.length,
-      xIsNext: !this.state.xIsNext
+      xIsNext: !this.state.xIsNext,
     });
   }
 
-  getHistory = () => {
-    return this.state.history.slice(0, this.state.stepNumber + 1);
-  }
+  getHistory = () => this.state.history.slice(0, this.state.stepNumber + 1);
 
   getCurrentHistory() {
     const history = this.getHistory();
@@ -82,7 +82,12 @@ class Game extends React.Component {
   updateStatus(current) {
     const winner = calculateWinner(current.squares);
     const hasNull = current.squares.some(value => value === null);
-    return winner ? `Winner ${winner}` : hasNull ? `Next ${this.whoIsNext()}` : 'Draw';
+    return winner ? `Winner ${winner.winner}` : hasNull ? `Next ${this.whoIsNext()}` : 'Draw';
+  }
+
+  updateColorWinnerMoves(current) {
+    const winner = calculateWinner(current.squares);
+    return winner ? winner.squares : null;
   }
 
   //atualiza o histório e quem é o próximo a jogar
@@ -91,11 +96,11 @@ class Game extends React.Component {
   getMoves() {
     const history = this.getHistory();
     const moves = history.map((step, move) => {
-      const desc = move ? `Ir para a jogada ${move}` : 'Reiniciar jogo';
-      const highlight =  move === this.state.stepNumber ? 'highlight': ''
+      const desc = move ? `Ir para a jogada ${move}` : 'Reiniciar o jogo';
+      const highlight = move === this.state.stepNumber ? 'highlight' : ''
       return (
         <li key={move}>
-          <button type="button" onClick={() => this.jumpTo(move)} className={ highlight ? 'btn btn-success' : 'btn btn-primary'}>{desc}</button>
+          <button type="button" onClick={() => this.jumpTo(move)} className={highlight ? 'btn btn-success' : 'btn btn-primary'}>{desc}</button>
         </li>
       )
     });
@@ -106,19 +111,21 @@ class Game extends React.Component {
   render() {
     const current = this.getCurrentHistory();
     const status = this.updateStatus(current);
+    const winnerMoves = this.updateColorWinnerMoves(current);
     const moves = this.getMoves();
-
+    
     return (
       <div className="game">
         <div className="game-board">
           <Board
             squares={current.squares}
+            winnerMoves={winnerMoves}
             onClick={(i) => this.handleClick(i)}
           />
         </div>
         <div className="game-info">
           <div>
-        <h3 className="text-info">{status}</h3>
+            <h3 className="text-info">{status}</h3>
           </div>
           <ol>{moves}</ol>
         </div>
@@ -141,8 +148,8 @@ function calculateWinner(squares) {
 
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    if (squares && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a]
+    if (squares && squares[a] !== null && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return { winner: squares[a], squares: [a,b,c] }
     }
   }
   return null;
