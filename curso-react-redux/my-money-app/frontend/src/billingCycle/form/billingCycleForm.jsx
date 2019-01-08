112ac8dd8ccadb75322d19cb3b8717702d1c54d5
Paulo Barros <paulobarros@Paulos-MacBook-Pro.local>
2019-01-08 20:57:31 -0200

    Curso React + Redux - Ciclo de Pagamento - Aula 156 - Componente Summary

--- curso-react-redux/my-money-app/frontend/src/billingCycle/form/billingCycleForm.jsx
+++ curso-react-redux/my-money-app/frontend/src/billingCycle/form/billingCycleForm.jsx
@@ -5,6 +5,7 @@ import { bindActionCreators } from 'redux';
 
 import LabelAndInput from './../../common/form/labelAndInput';
 import Button from '../../common/form/button';
+import Summary from './summary';
 import ItemList from './itemList';
 import { init } from './../billingCycleActions';
 
@@ -15,22 +16,24 @@ const CREDITS = 'credits'
 class BillingCycleForm extends Component {
     render() {
         const { handleSubmit, readOnly, submitLabel, submitClass, credits, debts } = this.props;
+
         return (
             <form role="form" onSubmit={handleSubmit}>
                 <div className="box-body">
                     <Field name="name" component={LabelAndInput} cols="12 4"
-                        label="Nome" placeholder="Informe o nome" readOnly={readOnly}/>
+                        label="Nome" placeholder="Informe o nome" readOnly={readOnly} />
                     <Field name="month" component={LabelAndInput} cols="12 4"
                         label="Mês" placeholder="Informe o mês" type="number" readOnly={readOnly} />
                     <Field name="year" component={LabelAndInput} cols="12 4"
                         label="Ano" placeholder="Informe o ano" type="number" readOnly={readOnly} />
-                    <ItemList cols='12 6'  list={credits} readOnly={readOnly} 
-                        legend="Creditos" field={CREDITS}/>
-                    <ItemList cols='12 6' list={debts} readOnly={readOnly} 
-                        legend="Débitos" field={DEBTS} showStatus={true}/>
+                    <Summary credit={1000} debt={100} />
+                    <ItemList cols='12 6' list={credits} readOnly={readOnly}
+                        legend="Creditos" field={CREDITS} />
+                    <ItemList cols='12 6' list={debts} readOnly={readOnly}
+                        legend="Débitos" field={DEBTS} showStatus={true} />
                 </div>
                 <div className="box-footer">
-                    <Button type="submit" style={submitClass} label={submitLabel}/>
+                    <Button type="submit" style={submitClass} label={submitLabel} />
                     <Button type="button" style="default" label="Cancelar"
                         onClick={this.props.init} />
                 </div>
@@ -45,10 +48,10 @@ BillingCycleForm = reduxForm({ form: BILLING_CYCLE_FORM, destroyOnUnmount: false
 const selector = formValueSelector(BILLING_CYCLE_FORM); //função usada para recuperar os valores do formulário
 // mapeando o estado para as props
 const mapStateToProps = state => (
-    { 
+    {
         credits: selector(state, CREDITS),
         debts: selector(state, DEBTS)
-    }); 
+    });
 
 // Fazendo a ligação (decorando) com as actions
 const mapDispatchToProps = dispatch => bindActionCreators({ init }, dispatch);
