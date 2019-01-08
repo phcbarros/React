85b807bec2b2f136a4f91e6631496a3ae3a00366
Paulo Barros <paulobarros@Paulos-MacBook-Pro.local>
2019-01-08 19:52:00 -0200

    Curso React + Redux - Ciclo de Pagamento - Aula 154- Generalizando o componente CreditList

--- curso-react-redux/my-money-app/frontend/src/billingCycle/form/billingCycleForm.jsx
+++ curso-react-redux/my-money-app/frontend/src/billingCycle/form/billingCycleForm.jsx
@@ -5,14 +5,16 @@ import { bindActionCreators } from 'redux';
 
 import LabelAndInput from './../../common/form/labelAndInput';
 import Button from '../../common/form/button';
-import CreditList from '../credit/creditList';
+import ItemList from './itemList';
 import { init } from './../billingCycleActions';
 
 export const BILLING_CYCLE_FORM = 'billingCycleForm';
+const DEBTS = 'debts';
+const CREDITS = 'credits'
 
 class BillingCycleForm extends Component {
     render() {
-        const { handleSubmit, readOnly, submitLabel, submitClass, credits } = this.props;
+        const { handleSubmit, readOnly, submitLabel, submitClass, credits, debts } = this.props;
         return (
             <form role="form" onSubmit={handleSubmit}>
                 <div className="box-body">
@@ -22,7 +24,10 @@ class BillingCycleForm extends Component {
                         label="Mês" placeholder="Informe o mês" type="number" readOnly={readOnly} />
                     <Field name="year" component={LabelAndInput} cols="12 4"
                         label="Ano" placeholder="Informe o ano" type="number" readOnly={readOnly} />
-                    <CreditList cols='12 6' list={credits} readOnly={readOnly} />
+                    <ItemList cols='12 6'  list={credits} readOnly={readOnly} 
+                        legend="Creditos" field={CREDITS}/>
+                    <ItemList cols='12 6' list={debts} readOnly={readOnly} 
+                        legend="Débitos" field={DEBTS}/>
                 </div>
                 <div className="box-footer">
                     <Button type="submit" style={submitClass} label={submitLabel}/>
@@ -38,7 +43,12 @@ class BillingCycleForm extends Component {
 BillingCycleForm = reduxForm({ form: BILLING_CYCLE_FORM, destroyOnUnmount: false, })(BillingCycleForm);
 
 const selector = formValueSelector(BILLING_CYCLE_FORM); //função usada para recuperar os valores do formulário
-const mapStateToProps = state => ({ credits: selector(state, 'credits')}); // mapeando o estado para as props
+// mapeando o estado para as props
+const mapStateToProps = state => (
+    { 
+        credits: selector(state, CREDITS),
+        debts: selector(state, DEBTS)
+    }); 
 
 // Fazendo a ligação (decorando) com as actions
 const mapDispatchToProps = dispatch => bindActionCreators({ init }, dispatch);
--- curso-react-redux/my-money-app/frontend/src/billingCycle/form/itemList.jsx
+++ curso-react-redux/my-money-app/frontend/src/billingCycle/form/itemList.jsx
@@ -6,26 +6,27 @@ import Grid from '../../common/layout/grid';
 import Input from '../../common/form/input';
 import IconButton from '../../common/form/iconButton';
 import If from '../../common/operator/if';
-import { BILLING_CYCLE_FORM } from '../form/billingCycleForm';
+import { BILLING_CYCLE_FORM } from './billingCycleForm';
 
-class CreditList extends Component {
+class ItemList extends Component {
 
     add(index, item = {}) {
-        this.props.arrayInsert(BILLING_CYCLE_FORM, 'credits', index, item);
+        this.props.arrayInsert(BILLING_CYCLE_FORM, this.props.field, index, item);
     }
 
-    remove(index, item = {}) {
+    remove(index) {
         if(this.props.list.length > 1)
-            this.props.arrayRemove(BILLING_CYCLE_FORM, 'credits', index);
+            this.props.arrayRemove(BILLING_CYCLE_FORM, this.props.field, index);
     }
 
     renderRows() {
+        const field = this.props.field;
         const list = this.props.list || [];
         return list.map((item, index) => (
             <tr key={index}>
-                <td><Field name={`credits[${index}].name`} component={Input}
+                <td><Field name={`${field}[${index}].name`} component={Input}
                     placeholder="Informe o nome" readOnly={this.props.readOnly}></Field></td>
-                <td><Field name={`credits[${index}].value`} component={Input}
+                <td><Field name={`${field}[${index}].value`} component={Input}
                     placeholder="Informe o valor" readOnly={this.props.readOnly}></Field></td>
                 <If test={!this.props.readOnly}>
                     <td>
@@ -42,7 +43,7 @@ class CreditList extends Component {
         return (
             <Grid cols={this.props.cols}>
                 <fieldset>
-                    <legend>Créditos</legend>
+                    <legend>{ this.props.legend }</legend>
                     <table className='table'>
                         <thead>
                             <tr>
@@ -65,4 +66,4 @@ class CreditList extends Component {
 
 const mapDispatchToProps = dispatch => bindActionCreators({ arrayInsert, arrayRemove }, dispatch);
 
-export default connect(null, mapDispatchToProps)(CreditList);
+export default connect(null, mapDispatchToProps)(ItemList);
