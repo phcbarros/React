cbf1045081ba14c754946c0b6b70585cc9f9b356
Paulo Barros <paulobarros@Paulos-MacBook-Pro.local>
2019-01-08 19:06:36 -0200

    Curso React + Redux - Ciclo de Pagamento - Aula 152 - Adicionar e clonar crédito

--- curso-react-redux/my-money-app/frontend/src/billingCycle/credit/creditList.jsx
+++ curso-react-redux/my-money-app/frontend/src/billingCycle/credit/creditList.jsx
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
-import { Field } from 'redux-form';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { Field, arrayInsert } from 'redux-form';
 import Grid from '../../common/layout/grid';
 import Input from '../../common/form/input';
+import IconButton from '../../common/form/iconButton';
+import If from '../../common/operator/if';
+import { BILLING_CYCLE_FORM } from '../form/billingCycleForm';
 
 class CreditList extends Component {
 
+    add(index, item = {}) {
+        this.props.arrayInsert(BILLING_CYCLE_FORM, 'credits', index, item);
+    }
+
     renderRows() {
-        const list = this.props.list || [{}];
+        const list = this.props.list || [];
         return list.map((item, index) => (
             <tr key={index}>
-                <td><Field name={`credits[${index}].name`} component={Input} 
-                    placeholder='Ínforme o nome' readOnly={this.props.readOnly}></Field></td>
-                <td><Field name={`credits[${index}].value`} component={Input} 
-                    placeholder='Ínforme o valor' readOnly={this.props.readOnly}></Field></td>
-                <td></td>
+                <td><Field name={`credits[${index}].name`} component={Input}
+                    placeholder="Informe o nome" readOnly={this.props.readOnly}></Field></td>
+                <td><Field name={`credits[${index}].value`} component={Input}
+                    placeholder="Informe o valor" readOnly={this.props.readOnly}></Field></td>
+                <If test={!this.props.readOnly}>
+                    <td>
+                        <IconButton style="success" icon="plus" onClick={() => this.add(index + 1)} type="button" />
+                        <IconButton style="warning" icon="clone" onClick={() => this.add(index + 1, item)} type="button" />
+                    </td>
+                </If>
             </tr>
         ));
     }
@@ -28,7 +42,9 @@ class CreditList extends Component {
                             <tr>
                                 <th>Nome</th>
                                 <th>Valor</th>
-                                <th>Ações</th>
+                                <If test={!this.props.readOnly}>
+                                    <th className="table-actions">Ações</th>
+                                </If>
                             </tr>
                         </thead>
                         <tbody>
@@ -41,4 +57,6 @@ class CreditList extends Component {
     }
 }
 
-export default CreditList;
+const mapDispatchToProps = dispatch => bindActionCreators({ arrayInsert }, dispatch);
+
+export default connect(null, mapDispatchToProps)(CreditList);
--- curso-react-redux/my-money-app/frontend/src/common/form/iconButton.jsx
+++ curso-react-redux/my-money-app/frontend/src/common/form/iconButton.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 export default props => (
     <button 
         className={`btn btn-${props.style}`}
+        type={props.type}
         onClick={props.onClick}>
         <i className={`fa fa-${props.icon}`}></i>
     </button>
