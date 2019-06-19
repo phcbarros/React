6676fec8de3e36b9cd2bac92ada59471081c0a26
Paulo Barrose <paulo.barros@lambda3.com.br>
2019-06-19 15:31:50 -0300

    Curso React + Redux - Ciclo de Pagamento - 177 - Autenticacao - Backend (Parte 08)

--- curso-react-redux/my-money-app/backend/src/config/routes.js
+++ curso-react-redux/my-money-app/backend/src/config/routes.js
@@ -1,12 +1,27 @@
-const express = require('express');
+const express = require('express')
+const auth = require('./auth')
 
-module.exports = server => {
+module.exports = (server) => {
+  /*
+   * Rotas protegidas por Token JWT
+   */
+  const protectedApi = express.Router()
+  server.use('/api', protectedApi)
 
-    //Definir URL base para todas as rotas
-    const router = express.Router() ;
-    server.use('/api', router);
+  protectedApi.use(auth)
 
-    // Rotas ciclo de pagamento
-    const billingCycle = require('../api/billingCycle/billingCycleService');
-    billingCycle.register(router, '/billingCycles');
+  // Rotas ciclo de pagamento
+  const billingCycle = require('../api/billingCycle/billingCycleService')
+  billingCycle.register(protectedApi, '/billingCycles')
+
+  /*
+   * Rotas abertas
+   */
+  const openApi = express.Router()
+  server.use('/oapi', openApi)
+
+  const AuthService = require('../api/user/authService')
+  openApi.post('/login', AuthService.login)
+  openApi.post('/signup', AuthService.signup)
+  openApi.post('/validateToken', AuthService.validateToken)
 }
