0cd52070eb1ef5b85f7ddeb15eb698fb7c951c2e
Paulo Barrose <paulo.barros@lambda3.com.br>
2019-06-19 15:21:23 -0300

    Curso React + Redux - Ciclo de Pagamento - 176 - Autenticacao - Backend (Parte 07)

--- curso-react-redux/my-money-app/backend/src/config/cors.js
+++ curso-react-redux/my-money-app/backend/src/config/cors.js
@@ -1,6 +1,12 @@
 module.exports = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS, PATCH');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-};
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, PUT, POST, DELETE, OPTIONS, PATCH',
+  )
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  )
+  next()
+}
