049355fa8b984f625b7838fa7cfcedab26b8a2e3
Paulo Barrose <paulo.barros@lambda3.com.br>
2019-06-19 14:31:58 -0300

    Curso React + Redux - Ciclo de Pagamento - 174 - Autenticacao - Backend (Parte 05)

--- curso-react-redux/my-money-app/backend/src/config/user/authService.js
+++ curso-react-redux/my-money-app/backend/src/config/user/authService.js
@@ -13,4 +13,28 @@ const sendErrorFromDB = (res, dbErrors) => {
   return res.status(400).json({ errors })
 }
 
-module.exports = {}
+const login = (req, res, next) => {
+  const email = req.body.email || ''
+  const password = req.body.password || ''
+
+  User.findOne({ email }, (err, user) => {
+    if (err) {
+      return sendErrorFromDB(err)
+    } else if (user && bcrypt.compareSync(password, user.password)) {
+      const token = jwt.sign(user, env.authSecret, {
+        expiresIn: '1 day',
+      })
+      const { name, email } = user
+      res.json({ name, email, token })
+    } else {
+      return res.status(400).send({ errors: ['Usuário/Senha inválidos'] })
+    }
+  })
+}
+
+const validateToken = (req, res, next) => {
+  const token = req.body.token || ''
+  jwt.verify(token, env.authSecret, (err, decoded) => {
+    res.status(200).send({ valid: !err })
+  })
+}
