09d2e3c4500c58d6e180e2bedf32e9a360b879fe
phcbarros <paulo.barros@outlook.com.br>
2021-04-23 18:27:10 -0300

    Curso React + Redux - 5 React Hooks - 58 useState #01

--- curso-react-redux/hooks/src/views/examples/UseState.jsx
+++ curso-react-redux/hooks/src/views/examples/UseState.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseState = (props) => {
-    return (
-        <div className="UseState">
-            <PageTitle
-                title="Hook UseState"
-                subtitle="Estado em componentes funcionais!"
-            />
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="UseState">
+      <PageTitle
+        title="Hook UseState"
+        subtitle="Estado em componentes funcionais!"
+      />
+      <SectionTitle title="Exercício 01" />
+      <div className="center">
+        <span className="text">{count}</span>
+        <div>
+          <button
+            className="btn"
+            onClick={() => setCount((current) => current + 1)}>
+            +1
+          </button>
+          <button
+            className="btn"
+            onClick={() => setCount((current) => current - 1)}>
+            -1
+          </button>
         </div>
-    )
+      </div>
+      <SectionTitle title="Exercício 02" />
+    </div>
+  )
 }
 
 export default UseState
