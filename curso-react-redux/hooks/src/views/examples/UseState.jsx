24eedb8de999298f02ea7c3e22dc3391f16915c6
phcbarros <paulo.barros@outlook.com.br>
2021-04-23 18:33:36 -0300

    Curso React + Redux - 5 React Hooks - 59 useState #02

--- curso-react-redux/hooks/src/views/examples/UseState.jsx
+++ curso-react-redux/hooks/src/views/examples/UseState.jsx
@@ -4,6 +4,7 @@ import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseState = (props) => {
   const [count, setCount] = useState(0)
+  const [name, setName] = useState()
 
   return (
     <div className="UseState">
@@ -28,6 +29,12 @@ const UseState = (props) => {
         </div>
       </div>
       <SectionTitle title="Exercício 02" />
+      <input
+        type="text"
+        className="input"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
     </div>
   )
 }
