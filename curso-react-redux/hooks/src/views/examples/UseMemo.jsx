b6b45ecdcb52e95d9d1960e1357c1bb5ae7ee10b
phcbarros <paulo.barros@outlook.com.br>
2021-05-01 19:12:18 -0300

    Curso React + Redux - 5 React Hooks - 66 useMemo

--- curso-react-redux/hooks/src/views/examples/UseMemo.jsx
+++ curso-react-redux/hooks/src/views/examples/UseMemo.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
+function sum(n1, n2) {
+  const future = Date.now() + 2000
+  while (future > Date.now()) {}
+  return n1 + n2
+}
+
 const UseMemo = (props) => {
-    return (
-        <div className="UseMemo">
-            <PageTitle
-                title="Hook UseMemo"
-                subtitle="Retorna um valor memoizado!"
-            />
-        </div>
-    )
+  const [n1, setN1] = useState(0)
+  const [n2, setN2] = useState(0)
+  const [n3, setN3] = useState(0)
+
+  const result = useMemo(() => sum(n1, n2), [n1, n2]) //memoiza o resultado da funçao até o valor das dependencias serem alterados
+
+  return (
+    <div className="UseMemo">
+      <PageTitle title="Hook UseMemo" subtitle="Retorna um valor memoizado!" />
+      <div className="center">
+        <input
+          type="number"
+          className="input"
+          value={n1}
+          onChange={(e) => setN1(parseInt(e.target.value))}
+        />
+        <input
+          type="number"
+          className="input"
+          value={n2}
+          onChange={(e) => setN2(parseInt(e.target.value))}
+        />
+        <input
+          type="number"
+          className="input"
+          value={n3}
+          onChange={(e) => setN3(parseInt(e.target.value))}
+        />
+        <span className="text">{result}</span>
+      </div>
+    </div>
+  )
 }
 
 export default UseMemo
