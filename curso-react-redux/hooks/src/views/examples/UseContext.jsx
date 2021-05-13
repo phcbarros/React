28d4d15ade24f382883a21cb6139b10c4118c11e
phcbarros <paulo.barros@outlook.com.br>
2021-05-13 18:40:20 -0300

    Curso React + Redux - 5 React Hooks - 69 UseContext #01

--- curso-react-redux/hooks/src/views/examples/UseContext.jsx
+++ curso-react-redux/hooks/src/views/examples/UseContext.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import DataContext from '../../data/DataContext'
 
 const UseContext = (props) => {
-    return (
-        <div className="UseContext">
-            <PageTitle
-                title="Hook UseContext"
-                subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
-            />
+  const context = useContext(DataContext)
+
+  function addNumber(delta) {
+    const number = context.state.number + delta
+    context.setState({ ...context.state, number })
+  }
+
+  return (
+    <div className="UseContext">
+      <PageTitle
+        title="Hook UseContext"
+        subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
+      />
+      <div className="center">
+        <span className="text">{context.state.text}</span>
+        <span className="text">{context.state.number}</span>
+        <div>
+          <button className="btn" onClick={() => addNumber(-1)}>
+            -1
+          </button>
+          <button className="btn" onClick={() => addNumber(1)}>
+            +1
+          </button>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default UseContext
